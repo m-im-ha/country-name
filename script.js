d893690c09b8c1df5477e8fa3eb42d66a466b718
Imran <sherlockholmesad221@gmail.com>
2024-01-30 01:28:06 +0600

    edit all files

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ async function find_country(country) {
     );
     let data = await response.json();
     console.log(data);
+    const native = Object.keys(data[0]["name"]["nativeName"]);
+    console.log(data[0]["name"]["nativeName"][`${native}`]["official"]);
+    console.log(data[0]["capital"]);
+    document.querySelector(".capital_name").textContent = data[0]["capital"]
+    console.log(data[0]["population"]);
+    const ppl = data[0]["population"];
+    console.log(new Intl.NumberFormat().format(ppl));
+    console.log(data[0]["languages"][`${native}`]);
+    const currency = Object.keys(data[0]["currencies"]);
+    console.log(data[0]["currencies"][`${currency}`]["name"]);
+    console.log(data[0]["region"]);
+    console.log(data[0]["subregion"]);
     console.log(data[0]["flags"]["svg"]);
   } catch (error) {
     console.error(error);
   }
 }
 find_country("bangladesh");
-find_country("turkey");
+// find_country("turkey");
